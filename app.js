40581b637a5672e7a8137da36be3f1a22966a5d2
Odai Qudah <odai92@live.com>
2021-01-06 19:37:30 +0200

    update

--- app.js
+++ app.js
@@ -1,94 +1,159 @@
 'use strict'
-
-
-var workingHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', 
-'11:00am', '12:00pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 am', '8:00 am', 'Daily Total'];
-var minCus = [];
-var maxCus = [];
-var avgCoSale = [Math.floor(Math.random() * 10)];
-var locations = ['seattle', 'tokyo','dubai','paris','lima'];
-
-console.log(avgCoSale);
-
-// here i made the objects for the five branchs, i still need to make the for loop to generate a random number, then calculate the sales for each hour using another for loop then render the list
-var seattle = {
-      locations:"seattle",
-      minCus:23 ,
-      maxCus:65 ,
-      avgCoSale:6.3,
-      
-       
+//creating a global hours array and a function to grnerate a random number of customers per hour
+var hours = ["6:00am", "7:00am", "8:00am", " 9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm"]
+var locationArray = [];
+function randomNumOfCustomers(minimum, maximum) {
+    return Math.floor(Math.random() * (maximum - minimum) + minimum);
 }
-seattle.sellPerHour = Math.floor(Math.random() * 10);
 
 
-var myArray = Object.values(seattle);
-document.getElementById("odai").innerHTML = myArray;
-
-console.log(Math.random());// so this makes a random number!!
-console.log(Math.random()*10);// and when multiply by ten we got a number between zero and ten
-console.log(Math.floor(Math.random() * 10));// using floor gave me a fixed number,,
-//how do i generate a random number and return the value to mmy array, i need to use the min and max values and multiply them with the random number then return them result of the calculation sum m3 b3d to the avg coki sale per hour
-// i will try to describe the function to make that inside the first branch witch is seattle use for 
-//==================
-//function randomNumber (minCus,maxCus){
-// minCus = Math.ceil(minCus);
-// maxCus = Math.floor(maxCus);
-// var result = Math.floor(Math.random()*(maxCus - minCus + 1) + minCus);
-// return result;
-// }
-// console.log(randomNumber);
-//==============
-// didnt work as what i think,
-//=============
-// ok after generating a number in a way or another i have to show an unorderlist for each citie showing the time from 6 am to 7pm and the sales per hour, ya halawa
+function Location(min, max, average, name) {
+    this.minCustomers = min;
+    this.maxCustomers = max;
+    this.averageCookiesSold = average;
+    this.locationName = name;
+    this.cookiesPerHour = [];
+    this.total = 0;
+    locationArray.push(this);
+}
 
 
+
+Location.prototype.numOfCustomers = function () {
+    return randomNumOfCustomers(this.minCustomers, this.maxCustomers);
+}
+
+
+
+
+Location.prototype.calAverCookiePerHour = function () {
+    
+    for (var i = 0; i < hours.length; i++) {
+        this.cookiesPerHour.push(Math.floor(this.numOfCustomers() * this.averageCookiesSold));
+    }
+
+   
+}
+
+
+
+
+function createTable() {
+    var parentElement = document.getElementById("TableOfData");
+    console.log(parentElement);
+    var article = document.createElement('article');
+    parentElement.appendChild(article);
+    var h2 = document.createElement('h2');
+    h2.textContent = "Locations: ";
+    article.appendChild(h2);
+    // start of the table
+    var table = document.createElement('table');
+    article.appendChild(table);
+}
+
+
+
+function firstHeaderRender() {
+    // adding headings (first row)
+    var parentElement = document.getElementsByTagName('table');
+    var tableFirstRow = document.createElement('tr');
+    parentElement[0].appendChild(tableFirstRow);
+
+    var headerEmpty = document.createElement('th');
+    headerEmpty.textContent = '';
+    tableFirstRow.appendChild(headerEmpty);
+
+    for (var i = 0; i < hours.length; i++) {
+        var header = document.createElement('th');
+        header.textContent = hours[i];
+        tableFirstRow.appendChild(header);
+    }
+    var headerDailyTotal = document.createElement('th');
+    headerDailyTotal.textContent = 'Daily Location Total';
+    tableFirstRow.appendChild(headerDailyTotal);
+}
+
+
+
+
+Location.prototype.renderLocationInTable = function () {
+    var parentElement = document.getElementsByTagName('table');
+    console.log(parentElement);
+
+    var tableSecondRow = document.createElement('tr');
+    parentElement[0].appendChild(tableSecondRow);
+    var nameOfLoction = document.createElement('td');
+    nameOfLoction.textContent = this.locationName;
+    tableSecondRow.appendChild(nameOfLoction);
+
+    this.calAverCookiePerHour();
+
+    for (var i = 0; i < hours.length; i++) {
+        var tableData = document.createElement('td');
+        tableData.textContent = this.cookiesPerHour[i];
+        tableSecondRow.appendChild(tableData);
+        this.total += this.cookiesPerHour[i];
+    }
+
+    var dailyTotal = document.createElement('td');
+    dailyTotal.textContent = this.total;
+    tableSecondRow.appendChild(dailyTotal);
+}
+
+
+
+
+function lastRender() {
+    var parentElement = document.getElementsByTagName('table');
+    var tableFinalRow = document.createElement('tr');
+    parentElement[0].appendChild(tableFinalRow);
+    var dataCell = document.createElement('td');
+    dataCell.textContent = 'Totals';
+    tableFinalRow.appendChild(dataCell);
+
+    var finalTotal = 0;
+
+    for (var i = 0; i < hours.length; i++) {
+        var totalLocationPerHour =0;
+        var hourDataCell = document.createElement('td');
+        for(var j=0; j<locationArray.length;j++){
+            totalLocationPerHour+=locationArray[j].cookiesPerHour[i];
+        }
+        
+        //var hourlyTotal = Math.floor(seattle.calAverCookiePerHour()[i] + tokyo.calAverCookiePerHour()[i] + paris.calAverCookiePerHour()[i] + dubai.calAverCookiePerHour()[i] + lima.calAverCookiePerHour()[i]);
+        hourDataCell.textContent = totalLocationPerHour;
+        tableFinalRow.appendChild(hourDataCell);
+        finalTotal += totalLocationPerHour;
+    }
+    var totalDataCell = document.createElement('td');
+    totalDataCell.textContent = finalTotal;
+    tableFinalRow.appendChild(totalDataCell);
+}
+
+
+
+
+
+
+var seattle = new Location(23, 65, 6.3, 'Seattle')
 console.log(seattle);
-
-var tokyo = {
-    locations:"tokyo",
-    minCus:3 ,
-    maxCus:24 ,
-    avgCoSale:1.2, 
-}
-tokyo.sellPerHour = Math.floor(Math.random() * 9);
-var myArray2 = Object.values(tokyo);
-document.getElementById("odai2").innerHTML = myArray2;
+console.log(seattle.calAverCookiePerHour());
+var tokyo = new Location(3, 24, 1.2, 'Tokyo');
 console.log(tokyo);
-
-var dubai = {
-    locations:"duabi",
-    minCus:11 ,
-    maxCus:38 ,
-    avgCoSale:3.7,   
-}
-dubai.sellPerHour = Math.floor(Math.random() * 12);
-var myArray3 = Object.values(dubai);
-document.getElementById("odai3").innerHTML = myArray3;
+var dubai = new Location(11, 38, 3.7, 'Dubai');
 console.log(dubai);
-
-var paris = {
-    locations:"paris",
-    minCus:20 ,
-    maxCus:38 ,
-    avgCoSale:2.3,
-}
-paris.sellPerHour = Math.floor(Math.random() * 7);
-var myArray4 = Object.values(paris);
-document.getElementById("odai4").innerHTML = myArray4;
+var paris = new Location(20, 38, 2.3, 'Paris');
 console.log(paris);
-
-
-
-var lima = {
-    locations:"lima",
-    minCus:2 ,
-    maxCus:16 ,
-    avgCoSale:4.6,    
-}
-lima.sellPerHour = Math.floor(Math.random() * 11);
-var myArray5 = Object.values(lima);
-document.getElementById("odai5").innerHTML = myArray5;
+var lima = new Location(2, 16, 4.9, 'Lima');
 console.log(lima);
 
+
+
+
+// attached to the sales html page 
+createTable();
+firstHeaderRender();
+for(var index=0; index<locationArray.length; index++){
+    locationArray[index].renderLocationInTable();
+}
+lastRender();
